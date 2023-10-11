71fd5c587aa5af7c34a8c0bb551d9333708f1a6c
Achmad Khosyi' A. R <114477225+Khosyi0@users.noreply.github.com>
2023-10-11 23:12:19 +0700

    Update DNSMASTER.-yudhis.sh

--- DNSMASTER.-yudhis.sh
+++ DNSMASTER.-yudhis.sh
@@ -126,41 +126,31 @@ service bind9 restart
 service bind9 stop
 
 # No 7
-echo 'options {
-        directory "/var/cache/bind";
-        allow-query{any;};
-        auth-nxdomain no;    # conform to RFC1035
-        listen-on-v6 { any; };
-};' > /etc/bind/named.conf.options
-
-mkdir /etc/bind/baratayuda
-cp /etc/bind/db.local /etc/bind/baratayuda/baratayuda.abimanyu.D06.com
-
-echo 'zone "abimanyu.D06.com" {
-    type slave;
-    masters { 192.194.1.3; }; // Masukan IP DNS Master
-    file "/var/lib/bind/abimanyu.D06.com";
-};
-zone "baratayuda.abimanyu.D06.com" {
-    type master;
-    file "/etc/bind/baratayuda/baratayuda.abimanyu.D06.com";
-};' > /etc/bind/named.conf.local
-
 echo ';
 ; BIND data file for local loopback interface
 ;
 $TTL    604800
-@       IN      SOA     baratayuda.abimanyu.D06.com. root.baratayuda.abimanyu.D06.com. (
+@       IN      SOA     abimanyu.D06.com. root.abimanyu.D06.com. (
                      2023100101         ; Serial
                          604800         ; Refresh
                           86400         ; Retry
                         2419200         ; Expire
                          604800 )       ; Negative Cache TTL
 ;
-@       IN      NS      baratayuda.abimanyu.D06.com.
-@       IN      A       192.194.1.2       ; IP DNSSLAVE
-www     IN      CNAME   baratayuda.abimanyu.D06.com.
-@       IN      AAAA    ::1' > /etc/bind/baratayuda/baratayuda.abimanyu.D06.com
+@       IN      NS      abimanyu.D06.com.
+@       IN      A       192.194.1.3       ; IP DNS Master
+www     IN      CNAME   abimanyu.D06.com.
+parikesit       IN      A       192.194.3.2       ; IP abimanyu
+ns1     IN      A       192.194.1.3 ; IP DNSMASTER
+baratayuda      IN      NS      ns1
+@       IN      AAAA    ::1' > /etc/bind/jarkom/abimanyu.D06.com
+
+echo 'options {
+        directory "/var/cache/bind";
+        allow-query{any;};
+        auth-nxdomain no;    # conform to RFC1035
+        listen-on-v6 { any; };
+};' > /etc/bind/named.conf.options
 
 service bind9 restart
 
