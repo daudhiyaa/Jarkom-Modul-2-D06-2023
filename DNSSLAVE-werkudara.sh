01ab33e778ea1e4feef24e32b009a9f0701af93a
Achmad Khosyi' A. R <114477225+Khosyi0@users.noreply.github.com>
2023-10-11 23:13:42 +0700

    Update DNSSLAVE-werkudara.sh

--- DNSSLAVE-werkudara.sh
+++ DNSSLAVE-werkudara.sh
@@ -49,25 +49,25 @@ www     IN      CNAME   baratayuda.abimanyu.D06.com.
 service bind9 restart
 
 # No 8
-mkdir /etc/bind/jarkom 
-cp /etc/bind/db.local /etc/bind/jarkom/rjp.baratayuda.abimanyu.D06.com
+mkdir /etc/bind/baratayuda 
+cp /etc/bind/db.local /etc/bind/baratayuda/rjp.baratayuda.abimanyu.D06.com
 
-echo "
-;
+echo ';
 ; BIND data file for local loopback interface
 ;
-\$TTL   604800
-@       IN      SOA     rjp.baratayuda.abimanyu.D06.com. root.rjp.baratayuda.abimanyu.D06.com. (
-                        2023100101      ; Serial
-                            604800      ; Refresh
-                             86400      ; Retry
-                           2419200      ; Expire
-                            604800 )    ; Negative Cache TTL
+$TTL    604800
+@       IN      SOA     baratayuda.abimanyu.D06.com. root.baratayuda.abimanyu.D06.com. (
+                     2023100101         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         604800 )       ; Negative Cache TTL
 ;
-@       IN      NS      rjp.baratayuda.abimanyu.D06.com
-@       IN      A       192.194.1.2     ; IP DNSSLAVE
-www     IN      CNAME   rjp.baratayuda.abimanyu.D06.com
-@       IN      AAAA    ::1
-" >/etc/bind/delegasi/baratayuda.abimanyu.D06.com
+@       IN      NS      baratayuda.abimanyu.D06.com.
+@       IN      A       192.194.1.2       ; IP DNSSLAVE
+www     IN      CNAME   baratayuda.abimanyu.D06.com.
+rjp     IN      A       192.194.3.2       ; IP Abimanyu
+www.rjp  IN     CNAME   rjp.baratayuda.abimanyu.D06.com.
+@       IN      AAAA    ::1' > /etc/bind/baratayuda/baratayuda.abimanyu.D06.com
 
 service bind9 restart
