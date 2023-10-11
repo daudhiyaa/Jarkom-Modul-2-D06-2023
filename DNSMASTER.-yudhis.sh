af9a8cc372d7b58d7d2692da71843d2900e798b3
Achmad Khosyi' A. R <114477225+Khosyi0@users.noreply.github.com>
2023-10-11 11:39:54 +0700

    Update DNSMASTER.-yudhis.sh
    no 7

--- DNSMASTER.-yudhis.sh
+++ DNSMASTER.-yudhis.sh
@@ -123,3 +123,70 @@ zone \"1.194.192.in-addr.arpa\" {
 service bind9 restart
 
 service bind9 stop
+
+# No 7
+echo "
+;
+; BIND data file for local loopback interface
+;
+\$TTL   604800
+@       IN      SOA     abimanyu.D06.com. root.abimanyu.D06.com. (
+                        2023100101      ; Serial
+                            604800      ; Refresh
+                             86400      ; Retry
+                           2419200      ; Expire
+                            604800 )    ; Negative Cache TTL
+;
+@       IN      NS      abimanyu.D06.com.
+@       IN      A       192.194.1.3     ; IP DNSMASTER
+www     IN      CNAME   abimanyu.D06.com.
+parikesit IN    A       192.194.3.2     ; IP Abimanyu
+ns1	IN	A	192.194.1.2	; IP DNSSLAVE
+baratayuda IN	NS	ns1
+@       IN      AAAA    ::1
+" >/etc/bind/jarkom/abimanyu.D06.com
+
+echo "
+options {
+    directory "/var/cache/bind/";
+
+    // If there is a firewall between you and nameservers you want
+    // to talk to, you may need to fix the firewall to allow multiple
+    // ports to talk. See http://www.kb.cert.org/vuls/id/800113
+
+    // If your ISP provided one or more IP addresses for stable
+    // nameservers, you probably want to use the as forwarders.
+    // Uncomment the following block, and insert the addresses replacing
+    // the all-0's placeholder.
+
+    // forwarders {
+    //      0.0.0.0;
+    // };  
+
+    //=======================================================================
+    // If BIND logs error messages about the root keu being expired,
+    // you will need to update your keys. See https://www.isc.org/bind-keys
+    //=======================================================================
+    //dnssec-validation auto;
+    allow-query{any;};
+
+    auth-nxdomain no;    # conform to RFC1035
+    listen-on-v6 { any; };
+};
+" > /etc/bind/named.conf.options
+
+nano /etc/bind/named.conf.local
+echo "
+zone \"abimanyu.D06.com\" {
+    type master;
+    file \"/etc/bind/jarkom/abimanyu.D06.com\";
+    allow-transfer { 192.194.1.2; }; # IP DNSSLAVE
+};
+
+zone \"1.194.192.in-addr.arpa\" {
+  type master; 
+  file \"/etc/bind/jarkom/1.194.192.in-addr.arpa\"; 
+};
+" >/etc/bind/named.conf.local
+
+service bind9 restart
