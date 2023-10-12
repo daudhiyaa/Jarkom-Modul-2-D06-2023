b4ba761f52a1b1529859de84388e2147939c428e
Daud Dhiya <daud.dhiya31@gmail.com>
2023-10-12 20:25:48 +0700

    feat: no 15

---
+++ 15/CLIENT.sh
@@ -0,0 +1,2 @@
+lynx parikesit.abimanyu.D06.com/error          # untuk cek 403
+lynx parikesit.abimanyu.D06.com/error/404.html # untuk cek 404
---
+++ 15/Webserver-Yudhistira.sh
@@ -0,0 +1,37 @@
+echo -e '
+<html
+  style="
+    background-image: url('nyee.jpg');
+    height: 100;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    height: 100%;
+    width: 100%;
+  "
+>
+  <header>
+    <h1 style="text-align: center; color: white">
+      lmao ERROR 403 (Forbidden) BRO dari kelompok D06
+    </h1>
+  </header>
+</html>
+' >/var/www/parikesit.abimanyu.D06/error/403.html
+
+echo -e '
+<html
+  style="
+    background-image: url('nyee.jpg');
+    height: 100;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    height: 100%;
+    width: 100%;
+  "
+>
+  <header>
+    <h1 style="text-align: center; color: white">
+      lmao ERROR 404 (Forbidden) BRO dari kelompok D06
+    </h1>
+  </header>
+</html>
+' >/var/www/parikesit.abimanyu.D06/error/404.html
