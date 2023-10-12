f5ccca14a2976177a2b3b47ce6d6244d3c00c9b7
Daud Dhiya <daud.dhiya31@gmail.com>
2023-10-12 20:37:34 +0700

    fix: no 16

--- 16/Webserver-Yudhistira.sh
+++ 16/Webserver-Yudhistira.sh
@@ -1,4 +1,4 @@
 echo "
-Alias \"/js \" \"/var/www/parikesit.abimanyu.yyy.com/public/js\"
+Alias \"/js\" \"/var/www/parikesit.abimanyu.D06/public/js\"
 " >>/etc/apache2/sites-available/parikesit.abimanyu.D06.com.conf
 service apache2 restart
