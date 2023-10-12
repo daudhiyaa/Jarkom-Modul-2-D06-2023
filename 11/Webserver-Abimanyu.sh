75037408e2f8305d5be6e2d09ae66d1987d9ea78
Daud Dhiya <daud.dhiya31@gmail.com>
2023-10-12 19:36:10 +0700

    fix: no 11

--- 11/Webserver-Abimanyu.sh
+++ 11/Webserver-Abimanyu.sh
@@ -5,12 +5,9 @@ service apache2 start
 apt-get install php -y
 php -v
 
+apt-get install libapache2-mod-php7.0
+
 mkdir /var/www/abimanyu.D06
-echo "
-<?php
-echo \"Halo, Kamu berada di Apache Abimanyu\";
-?>
-" >/var/www/abimanyu.D06/index.php
 
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/abimanyu.D06.com.conf
 rm /etc/apache2/sites-available/000-default.conf
@@ -24,6 +21,14 @@ echo -e '<VirtualHost *:80>
   ErrorLog ${APACHE_LOG_DIR}/error.log
   CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>' >/etc/apache2/sites-available/abimanyu.D06.com.conf
+
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1a4V23hwK9S7hQEDEcv9FL14UkkrHc-Zc" -O /var/www/abimanyu.D06/abimanyu.D06.com.zip
+
+unzip /var/www/abimanyu.D06/abimanyu.D06.com.zip -d /var/www/abimanyu.D06/
+
+mv /var/www/abimanyu.D06/abimanyu.yyy.com/home.html /var/www/abimanyu.D06/
+mv /var/www/abimanyu.D06/abimanyu.yyy.com/abimanyu.webp /var/www/abimanyu.D06/
+mv /var/www/abimanyu.D06/abimanyu.yyy.com/index.php /var/www/abimanyu.D06/
 
 a2ensite abimanyu.D06.com.conf
 service apache2 reload
