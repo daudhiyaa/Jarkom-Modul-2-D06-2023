36b494e00acc6df78abb21824276e3b076f1eea1
Daud Dhiya <daud.dhiya31@gmail.com>
2023-10-12 19:44:27 +0700

    feat: no 12

---
+++ 12/Webserver-Abimanyu.sh
@@ -0,0 +1,8 @@
+echo "
+<Directory /var/www/abimanyu.D06/index.php/home>
+    Options +Indexes
+</Directory>
+
+Alias "/home" "/var/www/abimanyu.D06/index.php/home"
+" >>/etc/apache2/sites-available/abimanyu.D06.com.conf
+service apache2 restart
